bdd5afbebabaac64cd473b7c58e6a0a3e323d4be
karimi <behrouzkarimi1993@gmail.com>
2025-03-22 23:07:19 +0000

    print errors to stderr

--- projects/output-and-error-handling/main.go
+++ projects/output-and-error-handling/main.go
@@ -16,12 +16,14 @@ func handleWeatherRequest() (string, error) {
 	if err != nil {
 
 		if os.IsTimeout(err) {
+			fmt.Fprintf(os.Stderr ,"request timed out.\n");
 			return "", fmt.Errorf("request timed out")
 		}
 		if err == io.EOF {
+			fmt.Fprintf(os.Stderr ,"connection terminated unexpectedly.\n")
 			return "", fmt.Errorf("connection terminated unexpectedly")
 		}
-
+		fmt.Fprintf(os.Stderr ,"failed to fetch server: %v\n", err);
 		return "", fmt.Errorf("failed to fetch server: %v", err)
 	}
 
@@ -31,7 +33,7 @@ func handleWeatherRequest() (string, error) {
 		retryAfter := resp.Header.Get("Retry-After")
 
 		if retryAfter == "" {
-			fmt.Println("server is too busy, automatically retries after 2 seconds...")
+			fmt.Fprintf(os.Stderr,"server is too busy")
 
 			// 2 seconds is a good choice because it's short enough to minimize user delay while still giving the server a
 			//  brief moment to recover, avoiding overloading it with frequent requests.
@@ -44,29 +46,38 @@ func handleWeatherRequest() (string, error) {
 				retryTime,err:= time.Parse(time.RFC1123, retryAfter);
 
 				if err!=nil{
-					fmt.Println("Retry-After header is invalid, automatically retries after 2 seconds...")
+					fmt.Fprintf(os.Stderr, "error happened: %v\n" , err);
 					time.Sleep(2 * time.Second)
 					return handleWeatherRequest()
 				}
 
 				waitingDuration:=time.Until(retryTime);
 				if waitingDuration.Seconds() > 5 {
-				return "", fmt.Errorf("server response takes %.0f seconds, process canceled", waitingDuration.Seconds())
+
+					fmt.Fprintf(os.Stderr,"server response takes %.0f seconds, process canceled", waitingDuration.Seconds())
+
+					return "", fmt.Errorf("server response takes %.0f seconds, process canceled", waitingDuration.Seconds())
+
 				}else if waitingDuration.Seconds()>0{
-					fmt.Printf("Waiting for %.0f seconds...\n", waitingDuration.Seconds());
+
+					fmt.Fprintf(os.Stderr ,"Waiting for %.0f seconds...\n", waitingDuration.Seconds())
+
 					time.Sleep(waitingDuration);
 					return handleWeatherRequest();
 				}else{
-					fmt.Println("retrying to get the weather... please wait")
+					fmt.Fprintf(os.Stderr,"retrying to get the weather... please wait")
 				}
 
 				return handleWeatherRequest();
 			}
 
 			if retryAfterInSecond > 5 {
+				fmt.Fprintf(os.Stderr , "server response takes %d seconds, process canceled \n", retryAfterInSecond);
+
 				return "", fmt.Errorf("server response takes %d seconds, process canceled", retryAfterInSecond)
+
 			} else if retryAfterInSecond >= 1 && retryAfterInSecond <= 5 {
-				fmt.Printf("Waiting for %d seconds...\n", retryAfterInSecond)
+				fmt.Fprintf(os.Stderr , "Waiting for %d seconds...\n", retryAfterInSecond);
 				time.Sleep(time.Duration(retryAfterInSecond) * time.Second)
 				return handleWeatherRequest()
 			}
@@ -75,19 +86,23 @@ func handleWeatherRequest() (string, error) {
 	} else if resp.StatusCode == http.StatusOK {
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
+			fmt.Fprintf(os.Stderr , "couldn't read the body response: %v\n", err)
 			return "", fmt.Errorf("couldn't read the body response: %w", err)
 		}
 		return string(body), nil
 	} else {
+		fmt.Fprintf(os.Stderr ,"unkown status code:  %d .\n", resp.StatusCode)
 		return "", fmt.Errorf("unkown status code:  %d . ", resp.StatusCode)
 	}
-	return "", fmt.Errorf("unexpected error happened")
+
+	fmt.Fprintf(os.Stderr ,"unexpected error happened");
+	return "", fmt.Errorf("unexpected error happened");
 }
 
 func main() {
 	weather, err := handleWeatherRequest()
 	if err != nil {
-		fmt.Println("error happened: ", err)
+		fmt.Fprintf(os.Stderr,"error happened: %v\n",err);
 		os.Exit(1)
 	}
 
